483b33b46eece90fb900b2370b3168e1ebf94083
Daniel Bauer <daniel.bauer.x@gmail.com>
2020-05-11 23:34:11 +0200

    Abstract discord stuff into own context

--- src/BusfoanBot/BotContext.cs
+++ src/BusfoanBot/BotContext.cs
@@ -14,22 +14,19 @@ namespace BusfoanBot
 {
     public delegate EmbedBuilder MessageBuilder(EmbedBuilder builder);
 
-    public class BotContext : IContext<BotContext>, IXStateSerializable
+    public class BotContext : DiscordContext, IContext<BotContext>, IXStateSerializable
     {
         public BotContext(
             ISocketMessageChannel channel,
             IEnumerable<Question> questions)
+            : base(channel)
         {
-            Channel = channel ?? throw new ArgumentNullException(nameof(channel));
             AllPlayers = ImmutableList<Player>.Empty;
             Questions = ImmutableStack.CreateRange(questions.Reverse());
             Players = ImmutableStack<Player>.Empty;
             Cards = ImmutableStack<Card>.Empty;
             PlayerCards = new Dictionary<ulong, ImmutableList<Card>>();
         }
-
-        public ISocketMessageChannel Channel { get; }
-        public RestUserMessage LastReactableMessage { get; set; }
 
         public ImmutableStack<Card> Cards { get; set; }
         public IDictionary<ulong, ImmutableList<Card>> PlayerCards { get; }
@@ -58,47 +55,6 @@ namespace BusfoanBot
             int count = AllPlayers.Count;
             AllPlayers = AllPlayers.RemoveAll(p => p.Id == id);
             return AllPlayers.Count - count > 0;
-        }
-
-        public async Task<RestUserMessage> SendMessage(string message)
-        {
-            return await Channel.SendMessageAsync(message);
-        }
-        public async Task<RestUserMessage> SendMessage(MessageBuilder builder)
-        {
-            var embed = builder(new EmbedBuilder()).Build();
-            return await Channel.SendMessageAsync(null, embed: embed);
-        }
-
-        public async Task<RestUserMessage> SendReactableMessage(Embed message, params IEmote[] emotes)
-            => await this.SendReactableMessage(message, emotes.AsEnumerable());
-        public async Task<RestUserMessage> SendReactableMessage(Embed message, IEnumerable<IEmote> emotes)
-        {
-            LastReactableMessage = await Channel.SendMessageAsync(null, embed: message);
-            await LastReactableMessage.AddReactionsAsync(emotes.ToArray());
-            return LastReactableMessage;
-        }
-
-        public async Task UpdateMessage(RestUserMessage message, Embed content)
-        {
-            await message.ModifyAsync(prop =>
-            {
-                prop.Embed = content;
-            });
-        }
-        public async Task DeleteMessage(RestUserMessage message)
-        {
-            await message.DeleteAsync();
-        }
-
-        public async Task SendFile(string filePath)
-        {
-            await Channel.SendFileAsync(filePath);
-        }
-        public async Task SendFile(string filePath, MessageBuilder builder)
-        {
-            var embed = builder(new EmbedBuilder()).Build();
-            await Channel.SendFileAsync(filePath, embed: embed);
         }
 
         public bool AreQuestionsLeft => !Questions.IsEmpty;
---
+++ src/BusfoanBot/DiscordContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Rest;
+using Discord.WebSocket;
+
+namespace BusfoanBot
+{
+    public abstract class DiscordContext
+    {
+        public ISocketMessageChannel Channel { get; }
+        public RestUserMessage LastReactableMessage { get; set; }
+
+        protected DiscordContext(ISocketMessageChannel channel)
+        {
+            Channel = channel ?? throw new ArgumentNullException(nameof(channel));
+        }
+
+        public async Task<RestUserMessage> SendMessage(string message)
+        {
+            return await Channel.SendMessageAsync(message);
+        }
+        public async Task<RestUserMessage> SendMessage(MessageBuilder builder)
+        {
+            var embed = builder(new EmbedBuilder()).Build();
+            return await Channel.SendMessageAsync(null, embed: embed);
+        }
+
+        public async Task<RestUserMessage> SendReactableMessage(Embed message, params IEmote[] emotes)
+            => await this.SendReactableMessage(message, emotes.AsEnumerable());
+        public async Task<RestUserMessage> SendReactableMessage(Embed message, IEnumerable<IEmote> emotes)
+        {
+            LastReactableMessage = await Channel.SendMessageAsync(null, embed: message);
+            await LastReactableMessage.AddReactionsAsync(emotes.ToArray());
+            return LastReactableMessage;
+        }
+
+        public async Task UpdateMessage(RestUserMessage message, Embed content)
+        {
+            await message.ModifyAsync(prop =>
+            {
+                prop.Embed = content;
+            });
+        }
+        public async Task DeleteMessage(RestUserMessage message)
+        {
+            await message.DeleteAsync();
+        }
+
+        public async Task SendFile(string filePath)
+        {
+            await Channel.SendFileAsync(filePath);
+        }
+        public async Task SendFile(string filePath, MessageBuilder builder)
+        {
+            var embed = builder(new EmbedBuilder()).Build();
+            await Channel.SendFileAsync(filePath, embed: embed);
+        }
+    }
+}
