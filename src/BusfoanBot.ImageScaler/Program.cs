b84c4ead916145e817ee030070fc912baf5a26e5
Daniel Bauer <daniel.bauer.x@gmail.com>
2020-05-19 18:47:44 +0200

    Add svg scaler

---
+++ src/BusfoanBot.ImageScaler/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using Svg;
+
+namespace BusfoanBot.ImageScaler
+{
+    class Program
+    {
+        static readonly string inputPath = "C:/Dev/busfoan-bot/Assets/Cards/SVG-cards-1.3";
+        static readonly string outputName = $"_OUTPUT";
+
+        static void Main(string[] args)
+        {
+            var inputDirectory = new DirectoryInfo(inputPath);
+            var files = Directory.EnumerateFiles(inputDirectory.FullName, "*.svg");
+
+            int count = files.Count();
+            Console.WriteLine($"Found {count} svgs.");
+
+            var outputDirectory = Directory.CreateDirectory($"{inputPath}/{outputName}");
+
+            int i = 1;
+            foreach (var file in files)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(file);
+
+                var svg = SvgDocument.Open(file);
+                svg.Width = 250;
+                svg.Height = 363;
+
+                using (var bitmap = svg.Draw())
+                {
+                    bitmap.Save($"{outputDirectory.FullName}/{fileName}.png", ImageFormat.Png);
+                }
+
+                Console.WriteLine($"[{i++}/{count}] Proceed {fileName}.svg");
+            }
+        }
+    }
+}
